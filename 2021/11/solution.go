3bb78f42d57ae2d4446e36a44f1c72d37710831e
James Matthews <james.matthews@instacart.com>
2021-12-10 21:24:38 -0800

    day 11 cleanup

--- 2021/11/solution.go
+++ 2021/11/solution.go
@@ -1,50 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
-	"runtime"
 )
-
-type scanner struct {
-	file *os.File
-	sc   *bufio.Scanner
-	err  error
-}
-
-func (s *scanner) NextLine() (string, bool) {
-	if s.sc.Scan() {
-		return s.sc.Text(), true
-	} else {
-		return "", false
-	}
-}
-
-func (s *scanner) Finish() error {
-	if s.file != nil {
-		s.err = s.sc.Err()
-
-		s.file.Close()
-		s.file = nil
-		s.sc = nil
-	}
-	return s.err
-}
-
-func getInputScanner() *scanner {
-	_, thisFilePath, _, _ := runtime.Caller(0)
-	f, err := os.OpenFile(filepath.Join(filepath.Dir(thisFilePath), "input.txt"), os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		log.Fatalf("error opening input file: %v", err)
-	}
-	return &scanner{
-		file: f,
-		sc:   bufio.NewScanner(f),
-	}
-}
 
 func main() {
 	doBoth()
@@ -55,22 +14,7 @@ type Point struct {
 	col int
 }
 
-var (
-	octoState = [][]int{
-		{7, 7, 7, 7, 8, 3, 8, 3, 5, 3},
-		{2, 2, 1, 7, 2, 7, 2, 4, 7, 8},
-		{3, 3, 5, 5, 3, 1, 8, 6, 4, 5},
-		{2, 2, 4, 2, 6, 1, 8, 1, 1, 3},
-		{7, 1, 8, 2, 4, 6, 8, 6, 6, 6},
-		{5, 4, 4, 1, 6, 4, 1, 1, 1, 1},
-		{4, 7, 7, 3, 8, 6, 2, 3, 6, 4},
-		{5, 7, 1, 7, 1, 2, 5, 5, 2, 1},
-		{7, 5, 4, 2, 1, 2, 7, 7, 2, 1},
-		{4, 5, 7, 6, 6, 7, 8, 3, 4, 1},
-	}
-)
-
-func blinkFrom(blinker Point) {
+func blinkFrom(blinker Point, octoState [][]int) {
 	neighbors := []Point{
 		{row: blinker.row - 1, col: blinker.col - 1},
 		{row: blinker.row - 1, col: blinker.col},
@@ -86,15 +30,28 @@ func blinkFrom(blinker Point) {
 			// real point
 			octoState[n.row][n.col]++
 			if octoState[n.row][n.col] == 10 {
-				blinkFrom(n)
+				// if >10, it's already been triggered
+				blinkFrom(n, octoState)
 			}
 		}
 	}
 }
 
 func doBoth() {
-	blinks := 0
+	octoState := [][]int{
+		{7, 7, 7, 7, 8, 3, 8, 3, 5, 3},
+		{2, 2, 1, 7, 2, 7, 2, 4, 7, 8},
+		{3, 3, 5, 5, 3, 1, 8, 6, 4, 5},
+		{2, 2, 4, 2, 6, 1, 8, 1, 1, 3},
+		{7, 1, 8, 2, 4, 6, 8, 6, 6, 6},
+		{5, 4, 4, 1, 6, 4, 1, 1, 1, 1},
+		{4, 7, 7, 3, 8, 6, 2, 3, 6, 4},
+		{5, 7, 1, 7, 1, 2, 5, 5, 2, 1},
+		{7, 5, 4, 2, 1, 2, 7, 7, 2, 1},
+		{4, 5, 7, 6, 6, 7, 8, 3, 4, 1},
+	}
 
+	blinks := 0
 	foundSynchronizedBlink := false
 	for step := 1; step <= 100 || !foundSynchronizedBlink; step++ {
 		blinksThisStep := 0
@@ -103,7 +60,8 @@ func doBoth() {
 			for c := 0; c < 10; c++ {
 				octoState[r][c]++
 				if octoState[r][c] == 10 {
-					blinkFrom(Point{row: r, col: c})
+					// if >10, it's already been triggered
+					blinkFrom(Point{row: r, col: c}, octoState)
 				}
 			}
 		}
